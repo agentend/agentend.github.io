5ecf3107e3a9f89f872f887c9ee56593d94c066c
gok03 <gokul.0369@gmail.com>
2026-03-26 01:59:44 +0530

    deploy: static site build

---
+++ _next/static/chunks/app/docs/capabilities/page-581ec52b5dccced9.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[113,409,871,524,920,906,799,120,90,286],{3521:function(){}},function(n){n.O(0,[971,117,744],function(){return n(n.s=3521)}),_N_E=n.O()}]);
